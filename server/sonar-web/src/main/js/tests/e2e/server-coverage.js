aaf41a85ac11c9d8f468d880da62176e66834d76
Stas Vilchik <vilchiks@gmail.com>
2015-02-25 09:39:58 +0100

    better integration with ci

--- server/sonar-web/src/main/js/tests/e2e/server-coverage.js
+++ server/sonar-web/src/main/js/tests/e2e/server-coverage.js
@@ -1,9 +1,11 @@
 var express = require('express'),
     path = require('path'),
     errorhandler = require('errorhandler'),
-    serveStatic = require('serve-static');
+    serveStatic = require('serve-static'),
+    im = require('istanbul-middleware');
 
 var staticPath = path.join(__dirname, '../../../webapp');
+im.hookLoader(staticPath);
 
 var app = express();
 
@@ -12,6 +14,8 @@ app.set('view engine', 'jade');
 
 app.use(errorhandler({ dumpExceptions: true, showStack: true }));
 
+app.use(im.createClientHandler(staticPath));
+app.use('/coverage', im.createHandler());
 app.use('/', serveStatic(staticPath));
 
 app.get('/pages/:page', function (req, res) {
